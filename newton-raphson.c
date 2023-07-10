3ab6093305f90879e4c169cfa66ef1ec4c86cf53
b0tish <maharjan.saugat1@gmail.com>
2023-07-11 04:31:41 +0545

    Initial Commit

---
+++ newton-raphson.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<math.h>
+#define EPS 0.01 
+#define f(x) x*x*x-3*x-2 
+#define fd(x) 2*x-3 
+int main(){
+    int count = 1;
+    float x0, xn, fx, fdx, error;
+
+    printf("Input initial value of x:");
+    scanf("%f", &x0);
+    printf("\nIter\t   x0 \t          f(x0)\t           f'(x0) \t   Error\n");
+    do{
+        fx = f(x0);
+        fdx= fd(x0);
+        xn = x0 - fx/fdx; 
+        error= fabs((xn - x0) / xn);
+        printf("%d\t %f \t %f \t %f \t %f\n", count,x0, fx,fdx, error);
+        x0 = xn;
+        count++;
+    }while(error > EPS);
+    printf("Root = %f\n\n", xn);
+    return 0;
+}
---
+++ secant.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#define f(x) x*x-x-1
+#define EPS 0.0001
+
+int main(){
+  int iter = 1;
+  float x1,x2,x3,e,f1,f2;
+ 
+  printf("\nStep\t\t   x1\t\t   f(x1)\t\t   x2\t\t           f(x2)\t\t   x3\t\t          Error\n");
+  
+  do{
+    f1 = f(x1);
+    f2 = f(x2);
+    x3 = x2 - ((f2*(x2-x1)) / (f2-f1));
+    e = fabs((x3 - x2) / x3);
+
+    printf("%d\t\t%f\t%f\t\t%f\t\t%f\t\t%f\t\t%f\n",iter,x1,f1,x2,f2,x3,e);
+    
+   x1 = x2;
+    x2 = x3;
+    iter++;
+  }while(e > EPS);
+  
+  printf("\nRoot:%f\n\n\n",x3);
+
+    return 0;
+}
